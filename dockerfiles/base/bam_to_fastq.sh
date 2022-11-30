4e41c956eeb7d631278e65ea9c76543216a0a479
Michele Berselli <berse@MacBook-Pro-2.local>
2022-11-30 15:58:21 -0500

    .

--- dockerfiles/base/bam_to_fastq.sh
+++ dockerfiles/base/bam_to_fastq.sh
@@ -9,3 +9,10 @@ out_prefix=$3
 fastq_opt="-0 /dev/null -s /dev/null -n"
 
 samtools collate -@ $nthreads -O $input_bam | samtools fastq -1 $out_prefix.1.fastq -2 $out_prefix.2.fastq -@ $nthreads $fastq_opt - || { echo "Cannot convert bam to fastq."; exit 1; }
+
+# Save space
+rm -rf $input_bam
+
+# Compress fastqs
+pigz $out_prefix.1.fastq || { echo "Cannot compress $out_prefix.1.fastq"; exit 1; }
+pigz $out_prefix.2.fastq || { echo "Cannot compress $out_prefix.2.fastq"; exit 1; }
